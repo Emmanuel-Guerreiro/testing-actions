1abd0391a8b4589bfc193c15b55cd3b86acc26c9
Emmanuel Guerreiro <emmanuelguerreiro98@gmail.com>
2023-09-28 17:18:29 -0300

    Segundo cambio

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Probando actions</div>;
+  return <div className={styles.container}>Probando actions otro cambio</div>;
 }
